7d6a0302339d0ce142f52dd8eed639aabec295a6
Seymour Poler <seymourpoler@gmail.com>
2018-03-25 15:45:00 +0200

    ignore: format

--- test/squel/condition_sentence_builder_test.cr
+++ test/squel/condition_sentence_builder_test.cr
@@ -7,18 +7,24 @@ class ConditionSentenceBuilderTest < Minitest::Test
     def test_expression_with_and
         builder = SQUEL::ConditionSentenceBuilder.new
 
-        assert_equal "id = 'aqswde' AND age > 24", builder.and("id = 'aqswde'").and("age > 24").to_string
+        result = builder.and("id = 'aqswde'").and("age > 24").to_string
+
+        assert_equal "id = 'aqswde' AND age > 24", result
     end
 
     def test_expression_with_or
         builder = SQUEL::ConditionSentenceBuilder.new
 
-        assert_equal "id = 'aqswde' OR age > 24", builder.and("id = 'aqswde'").or("age > 24").to_string
+        result = builder.and("id = 'aqswde'").or("age > 24").to_string
+
+        assert_equal "id = 'aqswde' OR age > 24", result
     end
 
     def test_expression_with_combination_of_or_conditions
         builder = SQUEL::ConditionSentenceBuilder.new
 
-        assert_equal "name <> 'Fred' AND (id = 5 OR id = 6)", builder.and("name <> 'Fred'").or_begin().or("id = 5").or("id = 6").end().to_string
+        result = builder.and("name <> 'Fred'").or_begin().or("id = 5").or("id = 6").end().to_string
+
+        assert_equal "name <> 'Fred' AND (id = 5 OR id = 6)", result
     end
 end
